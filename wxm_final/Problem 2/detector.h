d8e8035b38c23c3602c33e78759ea490e7dff46f
xiataoyue <44740675+xiataoyue@users.noreply.github.com>
2022-01-20 14:45:08 +0800

    update

--- wxm_final/Problem 2/detector.h
+++ wxm_final/Problem 2/detector.h
@@ -1,6 +1,7 @@
 #ifndef DETECTOR_H
 #define DETECTOR_H
 #include <vector>
+#include <iostream>
 using namespace std;
 
 class Detector
@@ -12,5 +13,32 @@ public:
      */
     void maxDetector(vector<int> inputArray);
 };
+
+void Detector::maxDetector(vector<int> inputArray) {
+    if(inputArray.empty()) cout << "0" << endl;
+    int size = inputArray.size();
+    int arr[10000];
+    for(int i = 0; i < size; i++){
+        arr[i] = inputArray[i];
+    }
+
+    int max = INT8_MIN;
+    for(int i = 0; i < size; i++){
+        if(arr[i] == 0){
+            if(i > 0){
+                if(arr[i - 1] >= max) max = arr[i - 1];
+            }
+            if(i < size - 1){
+                if(arr[i + 1] >= max) max = arr[i + 1];
+            }
+            if(i == 0 && i + 1 <= size - 1) max = arr[i + 1];
+            if(i == size - 1 && i - 1 >= 0){
+                if(arr[i - 1] >= max) max = arr[i - 1];
+            }
+        }
+        else continue;
+    }
+    cout << max << endl;
+}
 
 #endif
